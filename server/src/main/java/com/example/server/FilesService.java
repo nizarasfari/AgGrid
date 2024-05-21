519c9dcae02b3e06cfee8eec2a9d65390ea8d073
Wrok <wrok@macbook-pro.mynetworksettings.com>
2024-05-21 17:00:39 -0400

    changes

---
+++ server/src/main/java/com/example/server/FilesService.java
@@ -0,0 +1,44 @@
+package com.example.server;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilesService {
+    public static void  createFile(String fileName){
+        File dir = new File("files");
+
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        File file = new File(dir + File.separator + fileName);
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+                throw new IOException("File already exists.");
+            }
+
+        }catch (IOException e){
+            System.out.println("File could not be created.");
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+    public static List<String> listFiles(){
+        File dir = new File("files");
+        File[] files = dir.listFiles();
+        List<String> fileNames = new ArrayList<>();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    fileNames.add(file.getName());
+                }
+            }
+        }
+
+        return fileNames;
+    }
+}
---
+++ server/src/main/java/com/example/server/HomeController.java
@@ -0,0 +1,50 @@
+package com.example.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import com.example.server.FilesService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class HomeController {
+
+    @PostMapping("/createFile")
+    public ResponseEntity<String> createFile(@RequestParam String fileName, @RequestParam  String fileExtension ){
+        String file = fileName+"."+fileExtension;
+        try {
+            FilesService.createFile(file);
+            return  ResponseEntity.ok("file created" + file);
+
+        }catch (Exception e){
+            if (e.getMessage().contains("File already exists")) {
+                return ResponseEntity.status(409).body("File already exists.");
+            } else {
+                return ResponseEntity.status(500).body("An error occurred: " + e.getMessage());
+            }
+        }
+    }
+
+    @GetMapping("/list/files")
+    public ResponseEntity<List<String>> listFiles() {
+        try {
+            List<String> fileNames = FilesService.listFiles();
+            return ResponseEntity.ok(fileNames);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(null);
+        }
+    }
+
+
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello, World!";
+    }
+}
